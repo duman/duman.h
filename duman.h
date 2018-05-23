b9c3c744aac62f08513d7b28b528b6e6ac4bfb88
Tuğberk Kaan Duman <kaan@dumanmail.com>
2018-05-23 15:32:36 +0300

    Update duman.h

--- duman.h
+++ duman.h
@@ -28,11 +28,13 @@
 using namespace std;
 namespace fs = experimental::filesystem;
 
-void replace_str(string& str, const string& from, const string& to) {
+void replace_str(string& str, const string& from, const string& to)
+{
 	if (from.empty())
 		return;
 	size_t start_pos = 0;
-	while ((start_pos = str.find(from, start_pos)) != string::npos) {
+	while ((start_pos = str.find(from, start_pos)) != string::npos)
+	{
 		str.replace(start_pos, from.length(), to);
 		start_pos += to.length();
 	}
